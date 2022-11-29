590000377d3be6bca990dca09de77a5d4ff65b23
Dev Uppin <devuppin@gmail.com>
2022-11-29 15:18:15 -0800

    added prompt and alert to user

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const darkModeTheme = document.querySelector(".dmode")
 
 function changeToDarkMode () {
-    document.body.style.background = "#3F4E4F"
+    document.body.style.background = '#36454F'
     
 }
 darkModeTheme.addEventListener('click', changeToDarkMode)
@@ -20,22 +20,44 @@ darkModeTheme.addEventListener('click', changeTextButton)
 
 function makeWhite(e) {
     if (e.target.tagName === "BUTTON") {
-        e.target.style.backgroundColor = "white";
+        e.target.style.backgroundColor = "black";
     }
 
 }
 darkModeTheme.addEventListener('click', makeWhite)
 
 darkModeTheme.addEventListener('click', function onClick(event) {
-  // 👇️ Change text color for clicked element only
-  event.target.style.color = 'black';
+  //Change text color for clicked element only
+  event.target.style.color = 'white';
 });
 
-function backToLight () { 
+function backToLight (e) { 
     if (darkModeTheme.textContent === 'Dark Mode') {
-        document.body.style.backgroundColor = '#ECE2D0'
+        document.body.style.backgroundColor = '#ECE2D0';
+        e.target.style.backgroundColor = '#6F6A87';
+        e.target.style.color = 'white'
+
 
     }
 
 }
 darkModeTheme.addEventListener('click', backToLight)
+
+const clickSave = document.querySelector(".tosave")
+
+function alertUser () {
+    prompt('Enter the title of the note:');
+    alert('Your note has been saved')
+    
+}
+
+clickSave.addEventListener('click', alertUser)
+
+const list = []
+document.getElementsByClassName("notes") = list
+
+function pushNote () {
+    list.push('one' , 'five');
+    document.getElementsByClassName("notes") = list
+}
+clickSave.addEventListener('click', pushNote)
