11202922c031e607a690d369315a1451d5899aca
Dev Uppin <devuppin@gmail.com>
2022-11-28 22:03:22 -0800

    lightmode working

--- scripts/script.js
+++ scripts/script.js
@@ -24,12 +24,18 @@ function makeWhite(e) {
     }
 
 }
-
 darkModeTheme.addEventListener('click', makeWhite)
-
-
 
 darkModeTheme.addEventListener('click', function onClick(event) {
   // 👇️ Change text color for clicked element only
   event.target.style.color = 'black';
 });
+
+function backToLight () { 
+    if (darkModeTheme.textContent === 'Dark Mode') {
+        document.body.style.backgroundColor = '#ECE2D0'
+
+    }
+
+}
+darkModeTheme.addEventListener('click', backToLight)
