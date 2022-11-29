44c990dd760cbc5075855e43a6c0655272ff8034
Dev Uppin <devuppin@gmail.com>
2022-11-28 21:52:20 -0800

    working dmmode button

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,35 @@
 const darkModeTheme = document.querySelector(".dmode")
 
 function changeToDarkMode () {
-    document.body.style.backgroundColor = "#3F4E4F"
-    alert("done")
+    document.body.style.background = "#3F4E4F"
+    
+}
+darkModeTheme.addEventListener('click', changeToDarkMode)
+
+function changeTextButton () {
+    if(darkModeTheme.textContent === "Dark Mode") {
+        darkModeTheme.textContent = "Light Mode";
+    }
+    else if (darkModeTheme.textContent === "Light Mode") {
+        darkModeTheme.textContent = "Dark Mode";
+    }
+}
+darkModeTheme.addEventListener('click', changeTextButton)
+
+
+
+function makeWhite(e) {
+    if (e.target.tagName === "BUTTON") {
+        e.target.style.backgroundColor = "white";
+    }
+
 }
 
-darkModeTheme.addEventListener('click', changeToDarkMode)
+darkModeTheme.addEventListener('click', makeWhite)
+
+
+
+darkModeTheme.addEventListener('click', function onClick(event) {
+  // 👇️ Change text color for clicked element only
+  event.target.style.color = 'black';
+});
